389b20a1a137b51f0c97a257121aa356754e1b6f
Admin Nourishdaa <admin@nourishdaa.in>
2023-04-14 21:39:13 +0530

    Upgraded to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,11 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-storysource",
-    "@storybook/addon-docs",
-    "@storybook/preset-scss",
-  ]
-}
+  "stories": ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-storysource", "@storybook/addon-docs", "@storybook/preset-scss", "@storybook/addon-mdx-gfm"],
+  docs: {
+    autodocs: true
+  },
+  framework: {
+    name: "@storybook/html-webpack5",
+    options: {}
+  }
+};
